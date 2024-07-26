4c9908dd13dbd5dd21ee64208f24d68d7d9231cd
Vitoria Garcia <vitoria_garcia_costa_m-tsapp1ma2223@cev.com>
2024-07-26 18:41:41 +0200

    Initial Commit

---
+++ JuegoDeMemoria_Vitoria_Garcia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JuegoDeMemoria_Vitoria_Garcia
+//
+//  Created by Vitoria Garcia on 26/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JuegoDeMemoria_Vitoria_Garcia/JuegoDeMemoria_Vitoria_GarciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuegoDeMemoria_Vitoria_GarciaApp.swift
+//  JuegoDeMemoria_Vitoria_Garcia
+//
+//  Created by Vitoria Garcia on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct JuegoDeMemoria_Vitoria_GarciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
